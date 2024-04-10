a9d294bd6e7770680dd567317940b34f31ee918f
nisitsolanki <solankinisitgujarat@gmail.com>
2024-04-10 05:34:47 +0530

    update

--- src/index.js
+++ src/index.js
@@ -9,9 +9,12 @@ const http = require('http');
 const socketIo = require('socket.io');
 const app = express();
 const multer =require("multer");
+const server = http.createServer(app);
+const io = socketIo(server);
 
 app.use(bodyParser.json());
 app.use(multer().any());
+
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to database'))
